6acc23a78d5a7ce16fb3499eeafd0fd485ef5b4f
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-04-02 14:23:20 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
     express.json()(req, res, next);
   }
 });
-app.use(cors({ origin: "https://barber-web-lovat.vercel.app/" }));
+app.use(cors());
 
 app.use(router);
 
